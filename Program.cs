a82e5b8e2b1c7c6b5c2809626004a8df835a1439
Tatiana Trufanova <tatiana.trufanova@kofax.com>
2022-05-31 14:41:05 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Hello();
+            GoodBye();
+
+        }
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    public static void GoodBye()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+
+}
